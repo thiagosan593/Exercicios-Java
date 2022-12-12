cf49894c4e5e9e88be17e2b9021dbe658861eb56
thiagosan593 <thiago_593@hotmail.com>
2022-12-12 15:12:18 -0300

    array

---
+++ Exercicios/src/exercicios/Arraysimples.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Faculdade
+ */
+public class Arraysimples {
+    
+    public static void main(String[] args) {
+        
+        String[] paises ={"BRASIL", "RUSSIA" ,"INDIA","CHINA"};
+        
+        System.out.println(paises[2]);
+    }
+}

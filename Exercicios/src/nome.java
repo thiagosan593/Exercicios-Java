e8e01ac34591e83005ce98bbf53a51a99336e407
thiagosan593 <thiago_593@hotmail.com>
2022-12-12 15:27:07 -0300

    nome

---
+++ Exercicios/src/nome.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Thiago Faculdade
+ */
+public class nome {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        String nome = tec.nextLine();
+        System.out.println("Olá " + nome + " Seja bem vindo");
+
+    }
+}

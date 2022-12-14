608b9c0a35ef5243a086a8bc88943dce05059fed
thiagosan593 <thiago_593@hotmail.com>
2022-12-14 10:07:44 -0300

    booleani

---
+++ Exercicios/src/exercicios/Booleano.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class Booleano {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 2;
+        b = 3;
+        c = 5;
+        System.out.println((a == b) || (c > a));
+    }
+}

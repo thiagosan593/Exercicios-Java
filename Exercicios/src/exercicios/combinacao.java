b3ac59635e16cd10a00785f7541675f6cd6148ad
thiagosan593 <thiago_593@hotmail.com>
2022-12-15 15:12:35 -0300

    Combinação

---
+++ Exercicios/src/exercicios/combinacao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class combinacao {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        for (int cc = 1; cc <= 3; cc++) {
+            for (int cc1 = 1; cc1 <= 3; cc1++) {
+                System.out.println(cc + " " + cc1);
+            }
+        }
+
+    }
+
+}

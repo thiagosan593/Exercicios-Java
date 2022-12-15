90a79638a42920f2057ff9851dd118dd8086f3bb
thiagosan593 <thiago_593@hotmail.com>
2022-12-15 15:17:13 -0300

    Combinação

---
+++ Exercicios/src/exercicios/Combinacaoo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class Combinacaoo {
+
+    public static void main(String[] args) {
+        int j;
+        for (int i = 1; i <= 9; i += 2) {
+            for (j = 7; j >= 5; j--) {
+                System.out.println("I = " + i + " J = " + j);
+            }
+
+        }
+
+    }
+}

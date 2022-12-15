ccdc6198b549be514b2d77625a693f4bf4606160
thiagosan593 <thiago_593@hotmail.com>
2022-12-15 16:53:31 -0300

    Soma

---
+++ Exercicios/src/exercicios/SomaMetodos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class SomaMetodos {
+
+    public static void main(String[] args) {
+        System.out.println("Começou O programa");
+        soma(5, 5);
+    }
+
+    static void soma(int a, int b) {
+        int s = a + b;
+        System.out.println("A soma é " + s);
+    }
+
+}

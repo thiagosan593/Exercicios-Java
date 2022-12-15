80d640240e1c713c8f7d4e1a2f02815d705e98b8
thiagosan593 <thiago_593@hotmail.com>
2022-12-15 15:25:44 -0300

    While

---
+++ Exercicios/src/exercicios/While.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class While {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        do {
+            System.out.println(cc);
+            cc++;
+        } while (cc < 4);
+
+    }
+}

a7aa24b7c086543ab3d7b578751c949412567ae9
thiagosan593 <thiago_593@hotmail.com>
2022-12-15 16:41:01 -0300

    Vetor

---
+++ Exercicios/src/exercicios/Vetor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class Vetor {
+
+    public static void main(String[] args) {
+        int n[] = {3, 2, 8, 7, 5, 4};
+        for (int cc = 0; cc <= 5; cc++) {
+            System.out.print(n[cc] + " ");
+        }
+
+    }
+
+}

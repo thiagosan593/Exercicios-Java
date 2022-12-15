e10ef41ee1a90085774df05b985e294869568d3b
thiagosan593 <thiago_593@hotmail.com>
2022-12-15 16:11:21 -0300

    For it

---
+++ Exercicios/src/exercicios/ForIt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Faculdade
+ */
+public class ForIt {
+
+    public static void main(String[] args) {
+        double v[] = {3.5, 2.75, 9, -4.5};
+        for (double valor : v) {
+            System.out.print(valor + " ");
+        }
+    }
+}

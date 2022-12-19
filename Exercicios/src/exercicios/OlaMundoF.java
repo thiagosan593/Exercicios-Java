e51995950c66e2791b97bf8d1259387dc62fecc4
thiagosan593 <thiago_593@hotmail.com>
2022-12-19 17:05:20 -0300

    Ola Mundo

---
+++ Exercicios/src/exercicios/OlaMundoF.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class OlaMundoF {
+
+    public static void main(String[] args) {
+
+        gerador();
+        System.out.println("Olá mundo");
+        gerador();
+
+    }
+
+    public static void gerador() {
+        System.out.println("--------------------------");
+    }
+
+}

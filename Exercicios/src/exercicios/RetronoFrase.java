ee375d302c2b4f179269076db68f41727fe197a5
thiagosan593 <thiago_593@hotmail.com>
2022-12-19 17:14:13 -0300

    Retorno Função

---
+++ Exercicios/src/exercicios/RetronoFrase.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Thiago Faculdade
+ */
+public class RetronoFrase {
+    public static void main(String[] args) {
+        f("BEM VINDO");
+    }
+
+    public static String f(String a) {
+        System.out.println("+-------------------------+");
+        System.out.println(a);
+        System.out.println("+-------------------------+");
+        return null;
+    }
+}
+
+

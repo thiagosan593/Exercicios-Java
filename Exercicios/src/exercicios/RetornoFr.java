3e17968df8196a97483bd3ee2a4b5f414f931755
thiagosan593 <thiago_593@hotmail.com>
2022-12-19 17:16:51 -0300

    Retorno Frase repetição

---
+++ Exercicios/src/exercicios/RetornoFr.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Santos
+ */
+public class RetornoFr {
+
+    public static void main(String[] args) {
+
+        f("OLA");
+
+    }
+
+    public static String f(String a) {
+
+        System.out.println("+-------------------------+");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(a);
+        }
+        System.out.println("+-------------------------+");
+        return null;
+
+    }
+
+}

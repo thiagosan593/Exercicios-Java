d9f1f350ebcf881ac675237b4bc707fca6219c5c
thiagosan593 <thiago_593@hotmail.com>
2022-12-21 11:07:27 -0300

    Vetor s

---
+++ Exercicios/src/exercicios/VetorS.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Thiago Faculdade
+ */
+public class VetorS {
+
+    public static void main(String[] args) {
+        int[] n = new int[7];
+
+        for (int i = 0; i < n.length; i++) {
+            n[i] = (int) 999;
+
+        }
+        for (int i = 0; i < n.length; i++) {
+            System.out.println(n[i]);
+        }
+
+    }
+}
